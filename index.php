21e0ee300f1498000b91227135c31b11f29eb1c0
Benedict Etzel <developer@beheh.de>
2014-12-10 13:59:45 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+	<head>
+		<title>beheh.de</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8">
+		<link rel="stylesheet" type="text/css" href="beheh.css">
+	</head>
+	<body>
+		<header>
+			<h1><a href="//beheh.de">Benedict Etzel</a></h1>
+		</header>
+		<div id="content">
+			<div class="column first">
+				<h2 class="social">Social</h2>
+				<ul>
+					<li><span><a href="https://github.com/beheh">GitHub</a></span></li>
+					<li><span><a href="https://facebook.com/benedict.etzel">Facebook</a></span></li>
+					<li><span><a href="https://twitter.com/BehEh">Twitter</a></span></li>
+				</ul>	
+			</div>
+		</div>
+		<?php echo file_get_contents('footer.txt'); ?>
+		<?php echo file_get_contents('tracking.txt'); ?>
+	</body>
+</html>
